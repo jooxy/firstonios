6cf3b0d1c4255457d5efb282b8ad44c47b867269
jooxy <vojigor27@gmail.com>
2020-06-20 17:04:04 +0300

    registration field

--- First on iOS/First on iOS/ViewController.swift
+++ First on iOS/First on iOS/ViewController.swift
@@ -9,13 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+  
+    @IBOutlet weak var usn: UITextField!
+    @IBOutlet weak var pass: UITextField!
+    
+    let log1: String = "Login1"
+    let log2: String = "Login2"
+    let pass1: String = "12345"
+    let pass2: String = "qwerty"
+    
+    
+    
+    
+    
+   
+    @IBAction func button(_ sender: Any) {
+    
+    
+        if usn.text == log1 && pass.text == pass1 {
+            let alert = UIAlertController(title: "Успех", message: "Вы вошли", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Click to back", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+           
+        }
+    
+    
+        
+        if usn.text == log2 && pass.text == pass2 {
+            let alert = UIAlertController(title: "Успех", message: "Вы вошли", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Click to back", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        let alert = UIAlertController(title: "Не успех", message: "Вы не вошли", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Click to back", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+        
+    
+    }
     @IBOutlet weak var button: UIButton!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
 
 }
 
